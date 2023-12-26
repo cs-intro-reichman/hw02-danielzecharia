9bd23fb17ee8b7b6275194b179fd9ec46ca4f5e1
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-26 18:25:18 +0200

    update

--- InOrder.java
+++ InOrder.java
@@ -3,7 +3,17 @@
  *  as long as they form a non-decreasing sequence.
  */
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
+public static void main (String[] args) {
+int max=0;
+int random = (int) (Math.random() * 10);
+while (max<10)
+{
+	if (max < random)
+	{
+		System.out.println(random);
+		max=random;
 	}
+  random = (int) (Math.random() * 10);
+}
+}
 }
--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,20 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
+public static void main (String[] args){
+String a = args[0];
+int n = a.length()-1;
+for (int i = 0; i < a.length(); i++)
+{
+	System.out.print(a.charAt(n));
+	n--; 
+}
+System.out.println ();
+int middle =  a.length();
+if(middle%2==0)
+	System.out.println( "The middle Character is "+ a.charAt(middle/2-1) );
+else
+	System.out.println( "The middle Character is "+ a.charAt(middle/2) );
 		//// Put your code here
-	}
+}
 }
