72fa66bd807f273fba925c367344a6cd712a67b9
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-27 17:24:59 +0200

    update

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,7 +2,19 @@
  *  Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
+public static void main(String[] args) {
+int a= Integer.parseInt(args[0]);
+for (int i=1; i<=a; i++)
+  {
+  	if (i%2==0)
+  		System.out.print(" ");
+  	for (int z=0; z<a; z++)
+  	{
+  		System.out.print("* ");
+  	}
+  	System.out.println();
+  	
+  }
+
 	}
 }
--- OneOfEach.java
+++ OneOfEach.java
@@ -4,7 +4,31 @@
  *  to have children until they have at least one child of each gender.
  */
 public class OneOfEach {
-	public static void main (String[] args) {
-		//// Put your code here
+public static void main (String[] args) {
+int a=0;
+ int sum=0;
+ boolean ifboy=false;
+ boolean ifgirl=false;
+
+ while ((ifboy==false)||(ifgirl==false))
+ {
+ 	a= (int)(Math.random()*2);
+ 	if (a==0)
+ 	{
+ 		ifboy=true;
+ 		System.out.print("b ");
+ 	}
+ 	else
+ 	{
+ 		ifgirl=true;
+ 		System.out.print("g ");
+ 	}
+ 	sum++;
+ }
+
+System.out.println();
+System.out.println("You made it... and you now have "+sum+" children.");
+	
 	}
+
 }
